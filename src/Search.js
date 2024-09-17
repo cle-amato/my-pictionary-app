8f2d3a9d52f847cb015348f791fa29515ab0cacc
cle-amato <147545231+cle-amato@users.noreply.github.com>
2024-09-17 19:50:20 +0100

    Added random word API and setup call and response handler

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,49 @@
-//import axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
+import GameStartedView from "./GameStartedView";
+import CountdownTimer from "./CountdownTimer";
+
 
 export default function Search() {
+ const [ wordGenerated, setWordGenerated ] = useState ("")
+
+ function handleResponse(response) {
+    console.log(response)
+    console.log(`word generated:${(response.data[0])}`)
+   
+    setWordGenerated(response.data[0])  
+}
+
+
+
 
 
 function generateWord(event) {
     event.preventDefault();
     console.log("generateWord triggered")
     
+    let apiUrl = "https://random-word-api.vercel.app/api?words=1" 
+
+    axios.get(apiUrl).then(handleResponse)
     
-    //use axios to call the AI api
-    //set status generated 
-    //show new section with word (use state/effect)
 }
-    
+
+if (wordGenerated) {
+    return (
+        <section>
+            <form>
+                <button onClick={generateWord}>Generate</button>
+                <h3>{wordGenerated}</h3>
+                <section>
+                    <GameStartedView />
+                    <CountdownTimer />
+                </section>
+            </form>
+        </section>
+    )
+
+} else 
+
 return (
     <section>
         <form>
@@ -20,6 +51,7 @@ return (
         </form>
     </section>
 )
+
 }
 
 //add timer component
